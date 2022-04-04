1b54761cfd39ef72b28dc6a1e107c2812c924e10
selamawit <selinagebreyohannes@gmail.com>
2022-04-05 00:32:03 +0400

    main

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,1 +1,4 @@
-https://alx-students.slack.com/files/U031U75N37U/F03A13BJ1GB/create_static_lib.sh?origin_team=T0195LMKD1R&origin_channel=D033UQ6B6M6
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
