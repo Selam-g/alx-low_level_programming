d379f43c07af71ac31bfe2206531fcf196a7814d
selamawit <selinagebreyohannes@gmail.com>
2022-04-13 18:11:02 +0400

    Main

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,15 @@
-#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name
- * @name: name to print
- * @f: pointer to the printing function
- */
+  * print_name - fills memory with a constant byte
+  * @name: is the name of the dog
+  * @f: is a funtion pointer
+  * Return: a void pointer
+  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (!name || !f)
-		return;
-	f(name);
+	if (name == NULL || f == NULL)
+		;
+	else
+		f(name);
 }
